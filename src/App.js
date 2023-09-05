2459e0d8b082b79e300afa1591a960704453ce04
Mauro Busso <mauro.busso12@gmail.com>
2023-09-05 17:37:56 +0100

    added file from branch

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import './App.css';
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import { BottomNav, NavBar, Sidebar } from './Components/Components';
 
-import { Account, Address, Community, Health, Mealplanner } from './Pages/Page';
+import { Account, Address, Community, Health, OrderHistory,  Mealplanner } from './Pages/Page';
+import {Home, Shop, BritishBrands, SubscriptionAndRewards, ContactUs } from './Pages/Page'
 
 function App() {
   return (
-    <div className='relative max-h-screen box-border'>
-    <NavBar/>
-    <BrowserRouter >
-      <Sidebar>
-        <Routes>
-          <Route path='/' element={<Account/>} />
-          <Route path='/address' element={<Address/>} />
-          <Route path='/health' element={<Health/>} />
-          <Route path='/meal_planner' element={<Mealplanner/>} />
-          <Route path='/community' element={<Community/>} />
-        </Routes>
-      </Sidebar>
-      <BottomNav/>
-    </BrowserRouter>
+    <div className=' relative max-h-screen box-border '>
+      <BrowserRouter >
+        <NavBar/>
+          <Routes>
+            <Route path='/home' element={<Home />} />
+            <Route path='/Shop' element={<Shop />} />
+            <Route path='/BritishBrands' element={<BritishBrands />} />
+            <Route path='/SubscriptionAndRewards' element={<SubscriptionAndRewards />} />
+            <Route path='/ContactUs' element={<ContactUs />} />
+          </Routes>
+        <Sidebar>
+          <Routes>
+            <Route path='/' element={<Account/>} />
+            <Route path='/address' element={<Address/>} />
+            <Route path='/health' element={<Health/>} />
+            <Route path='/meal_planner' element={<Mealplanner/>} />
+            <Route path='/OrderHistory' element={<OrderHistory/>} />
+            <Route path='/community' element={<Community/>} />
+          </Routes>
+        </Sidebar>
+        <BottomNav/>
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -1,6 +1,24 @@
+//paths for side bar
+
 export {default as Account} from "./Account"
 export {default as Address} from "./Address"
 export {default as Mealplanner} from "./MealPlanner"
 export {default as Health} from "./Health"
 export {default as Community} from "./Community"
-export {default as Home} from "./HomePage"
+export {default as OrderHistory} from "./OrderHistory"
+
+
+//paths for nav bar pages 
+
+export {default as Home} from "./Navbar_pages/Home"
+export {default as Shop} from "./Navbar_pages/Shop"
+export {default as BritishBrands} from "./Navbar_pages/BritishBrands"
+export {default as SubscriptionAndRewards} from "./Navbar_pages/SubscriptionAndRewards"
+export {default as ContactUs} from "./Navbar_pages/ContactUs"
+
+//icons paths
+
+// export {default as Account} from "./Account"
+// export {default as Account} from "./Account"
+// export {default as Account} from "./Account"
+// export {default as Account} from "./Account"
